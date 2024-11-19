d3e177c725f0d85d9b8f4df0e617c8a7a3b39a86
jhonnCorredor <jhoncorredormedina29@gmail.com>
2024-11-19 16:42:11 -0500

    Integracion modulo centro formacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,9 +10,10 @@ import { ViewsModule } from './Segurity/views/views.module';
 import { AuthModule } from './auth/auth/auth.module';
 import { DepartamentsModule } from './Parametrization/departaments/departaments.module';
 import { TrainingCentersModule } from './parametrization/training-centers/training-centers.module';
+import { CityModule } from './Parametrization/city/city.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:4040/sena'), UsersModule , ModulosModule, RolModule, ViewsModule, AuthModule, DepartamentsModule, TrainingCentersModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:4040/sena'), UsersModule , ModulosModule, RolModule, ViewsModule, AuthModule, DepartamentsModule, TrainingCentersModule , CityModule],
   controllers: [AppController],
   providers: [AppService],
 })
