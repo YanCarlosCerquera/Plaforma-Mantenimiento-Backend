250bf95c488cbb44fb24f047fb22aedff95bfe0c
jhonnCorredor <jhoncorredormedina29@gmail.com>
2024-12-09 10:04:33 -0500

    Integración consultas con filtro

--- src/Maintenance/application-maintenance/sms.service.ts
+++ src/Maintenance/application-maintenance/sms.service.ts
@@ -5,15 +5,9 @@ import { ConfigService } from 'src/Parametrization/config/config.service';
 
 @Injectable()
 export class InfobipService {
-<<<<<<< HEAD
   private baseUrl: string;
   private apiKey: string;
   private fromNumber: string;
-=======
-  private readonly baseUrl: string = 'https://d93n3l.api.infobip.com'; 
-  private readonly apiKey: string = '742b53b58015390644c8e0f7fc517906-25d23d03-ef60-4ab0-bed4-bc6c7563d2f7'; 
-  private readonly fromNumber: string = '+44 7491 163443';  
->>>>>>> 9ae5e83a3941a2205c8d9a4b0c636a56624c767b
 
   constructor(private readonly httpService: HttpService, private readonly configService: ConfigService) {
   }
@@ -25,13 +19,9 @@ export class InfobipService {
     this.fromNumber = config.number;
   }
 
-<<<<<<< HEAD
   async sendSms(to: string, text: string) {
     await this.loadConfig();
 
-=======
-  async sendSms(to: string, text: string ) {
->>>>>>> 9ae5e83a3941a2205c8d9a4b0c636a56624c767b
     const url = `${this.baseUrl}/sms/2/text/advanced`;
 
     const data = {
