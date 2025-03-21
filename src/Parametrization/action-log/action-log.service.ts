5fc834ab809495a8e370c032718e5f2a72b88383
jhonnCorredor <jhoncorredormedina29@gmail.com>
2025-03-21 11:05:50 -0500

    Pagination y populated actionlog

--- src/Parametrization/action-log/action-log.service.ts
+++ src/Parametrization/action-log/action-log.service.ts
@@ -5,11 +5,39 @@ import { GenericService } from 'src/Generic/generic.service';
 import { ActionLog } from './entities/action-log.entity';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { PaginatedResult } from 'src/Generic/pagination.dto';
 
 @Injectable()
 export class ActionLogService extends GenericService<ActionLog, CreateActionLogDto, UpdateActionLogDto> {
   constructor(@InjectModel(ActionLog.name) private actionLogModel: Model<ActionLog>) {
     super(actionLogModel)
+  }
+
+  async findAllPaginated(
+    filters: any, // Define el tipo correcto para los filtros
+    page = 1,
+    limit = 20,
+  ): Promise<PaginatedResult<ActionLog>> {
+    // Llama al método findAllPaginated del servicio genérico
+    const result = await super.findAllPaginated(filters, page, limit);
+
+    // Aplica los populates específicos para ActionLog
+    result.data = await this.actionLogModel.populate(result.data, [
+      {
+        path: 'userId',
+        select: 'name',
+        populate: {
+          path: 'assignedRol',
+          select: 'name',
+        },
+      },
+      {
+        path: 'moduloId',
+        select: 'name',
+      },
+    ]);
+
+    return result;
   }
 
   async findAll(): Promise<ActionLog[]> {
